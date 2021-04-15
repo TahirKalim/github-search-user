894a7b74517d70c6df1aa622396ccccb15ec544e
tahir kalim <thrkalim@gmail.com>
2021-04-16 01:02:05 +0200

    info component

--- src/context/context.js
+++ src/context/context.js
@@ -12,7 +12,7 @@ const GithubContext = React.createContext();
 
  const GithubProvider = ({children}) => {
      const [githubUser, setGithubUser] = useState(mockUser)
-     const [repos, setRepos] = useState(mockUser)
+     const [repos, setRepos] = useState(mockRepos)
      const [followers, setFollowers] = useState(mockFollowers)
      return(
      <GithubContext.Provider value={{githubUser,repos,followers}} >{children}</GithubContext.Provider>
